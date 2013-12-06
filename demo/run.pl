8eedf08f396f60f1ccc30aa06f67a35e254ab5cc
prontokan <kan@prontomarketing.com>
2013-12-06 07:28:01 +0700

    update the cammand

--- demo/run.pl
+++ demo/run.pl
@@ -1,1 +1,2 @@
-system("../code/run_experiment ../data/input/pilot/preprocessed_training ../data/input/pilot/preprocessed_testing");
+#system("../code/run_experiment ../data/input/pilot/preprocessed_training ../data/input/pilot/preprocessed_testing");
+system("../code/run_experiment ../data/input/real/ex2/training ../data/input/real/ex2/testing");
